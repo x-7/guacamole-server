f4afa306d2a8778128548698f19875c537263f53
Nick Couchman <vnick@apache.org>
2020-04-05 16:42:14 -0400

    [WIP] This one is not working quite right..

--- src/protocols/rdp/channels/rdpsnd/rdpsnd.c
+++ src/protocols/rdp/channels/rdpsnd/rdpsnd.c
@@ -44,8 +44,13 @@ void guac_rdpsnd_process_receive(guac_rdp_common_svc* svc,
     Stream_Seek_UINT8(input_stream);
     Stream_Read_UINT16(input_stream, header.body_size);
     
-    if (Stream_GetRemainingLength(input_stream) < header.body_size)
+    if (Stream_GetRemainingLength(input_stream) < header.body_size) {
+        guac_client_debug(svc->client, GUAC_LOG_DEBUG, "Not enough bytes in stream."
+                "  Remaining: %d, Body size: %d",
+                Stream_GetRemainingLength(input_stream),
+                header.body_size);
         return;
+    }
     
     /* 
      * If next PDU is SNDWAVE (due to receiving WaveInfo PDU previously),
