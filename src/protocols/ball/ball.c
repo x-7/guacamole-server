f9aaa78768c871eb911a1632368bcbb9069eaabe
Nick Couchman <vnick@apache.org>
2020-12-30 22:20:25 -0500

    TUTORIAL: Add ball to display.

--- src/protocols/ball/ball.c
+++ src/protocols/ball/ball.c
@@ -17,7 +17,10 @@
  * under the License.
  */
 
+#include "ball.h"
+
 #include <guacamole/client.h>
+#include <guacamole/layer.h>
 #include <guacamole/protocol.h>
 #include <guacamole/socket.h>
 #include <guacamole/user.h>
@@ -30,6 +33,10 @@ int ball_join_handler(guac_user* user, int argc, char** argv) {
 
     /* Get client associated with user */
     guac_client* client = user->client;
+    
+    /* Get ball layer from client data */
+    ball_client_data* data = (ball_client_data*) client->data;
+    guac_layer* ball = data->ball;
 
     /* Get user-specific socket */
     guac_socket* socket = user->socket;
@@ -46,6 +53,18 @@ int ball_join_handler(guac_user* user, int argc, char** argv) {
             GUAC_COMP_OVER, GUAC_DEFAULT_LAYER,
             0x80, 0x80, 0x80, 0xFF);
 
+    /* Set up ball layer */
+    guac_protocol_send_size(socket, ball, 128, 128);
+
+    /* Prepare a curve which covers the entire layer */
+    guac_protocol_send_rect(socket, ball,
+            0, 0, 128, 128);
+
+    /* Fill curve with solid color */
+    guac_protocol_send_cfill(socket,
+            GUAC_COMP_OVER, ball,
+            0x00, 0x80, 0x80, 0xFF);
+    
     /* Mark end-of-frame */
     guac_protocol_send_sync(socket, client->last_sent_timestamp);
 
@@ -57,13 +76,39 @@ int ball_join_handler(guac_user* user, int argc, char** argv) {
 
 }
 
+int ball_free_handler(guac_client* client) {
+
+    ball_client_data* data = (ball_client_data*) client->data;
+
+    /* Free client-level ball layer */
+    guac_client_free_layer(client, data->ball);
+
+    /* Free client-specific data */
+    free(data);
+
+    /* Data successfully freed */
+    return 0;
+
+}
+
+
 int guac_client_init(guac_client* client) {
 
+    /* Allocate storage for client-specific data */
+    ball_client_data* data = malloc(sizeof(ball_client_data));
+
+    /* Set up client data and handlers */
+    client->data = data;
+
+    /* Allocate layer at the client level */
+    data->ball = guac_client_alloc_layer(client);
+    
     /* This example does not implement any arguments */
     client->args = TUTORIAL_ARGS;
 
     /* Client-level handlers */
     client->join_handler = ball_join_handler;
+    client->free_handler = ball_free_handler;
 
     return 0;
 
